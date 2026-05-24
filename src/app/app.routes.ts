55e0ee32dd2fc6f980ab1310dc3b834d94b0d7ee
OtavioRV <oreisviana@gmail.com>
2026-05-24 16:30:49 -0300

    feat: create authGuard and redirectIfAuthenticatedGuard, wire login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router'
+import { authGuard, redirectIfAuthenticatedGuard } from './core/auth/auth.guard'
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
+    canActivate: [redirectIfAuthenticatedGuard],
     loadComponent: () =>
       import('./shared/layouts/auth-layout/auth-layout.component').then(m => m.AuthLayoutComponent),
     children: [
       {
-        path: 'login',
+        path: '',
         loadComponent: () =>
           import('./features/auth/login/login.component').then(m => m.LoginComponent),
       },
